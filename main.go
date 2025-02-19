bb780b90ef815969b14bc86c1f03fd2bb54dd5c5
Matteo Pampana <matteopampana@gmail.com>
2025-02-19 19:57:41 +0100

    add example with iszero method

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	example1()
 	example2()
 	example3()
+	example4()
 }
 
 func example1() {
@@ -64,4 +65,36 @@ func example3() {
 	}
 
 	fmt.Printf("result 3: %s\n", data)
+}
+
+type CanBeZero struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func (c CanBeZero) IsZero() bool {
+	return c.Key == "zero" && c.Value == "zero"
+}
+
+func example4() {
+	type Response struct {
+		ID        string    `json:"id"`
+		Feature   CanBeZero `json:"feature,omitzero"`
+		UpdatedAt time.Time `json:"updated_at,omitzero"`
+	}
+
+	resp := Response{
+		ID: "123",
+		Feature: CanBeZero{
+			Key:   "zero",
+			Value: "zero",
+		},
+	}
+
+	data, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("result 4: %s\n", data)
 }
